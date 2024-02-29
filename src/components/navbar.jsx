5c7e81ce23556d4e7c6053ba1043c9913249ba54
Harinder <nexttoeveryone428@gmail.com>
2024-02-29 23:03:12 +0530

    done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     return (
         <div className="Landingpage">
            <div className="nav_container fixed top-0 z-10">
-            // eslint-disable-next-line react/no-unescaped-entities 
+            {/*  eslint-disable-next-line react/no-unescaped-entities  */}
             <div className="nav_head"> Web<span>Dev</span></div>
             <nav className="navbar">
                 <Link className="nav_links"  href={"#"}  >Home</Link>
@@ -15,7 +15,7 @@ export default function Navbar() {
             
             </div>
             
-             // eslint-disable-next-line react/no-unescaped-entities 
+              {/* eslint-disable-next-line react/no-unescaped-entities  */}
             <h1 className="h-80 mt-20 text-white">LET'S <span>BUILD</span> TOGETHER</h1>
             
       </div>
